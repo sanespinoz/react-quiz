66356769426ac0003661f39fb459e8cb9e654c8e
sanespinoz <san.espinoz@gmail.com>
2025-07-13 09:39:25 -0300

    Configure deploy for both GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const isGithubPages = process.env.DEPLOY_TARGET === 'GH_PAGES';
+
 // https://vite.dev/config/
 export default defineConfig({
   //base: '/', // Cambiado a raíz para pruebas locales
   //base: '/react-quiz/', // Cambiado a raíz para Github Pages
-  base: './', // Cambiado a raíz para Vercel
+  //base: './', // Cambiado a raíz para Vercel
   plugins: [react()],
+  base: isGitHubPages ? '/react-quiz/' : '/', // 👉 base '/' para Vercel, '/react-quiz/' para GitHub Pages
 });
