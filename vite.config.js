428bf62839318fcaafdac74ef92bc90c0bc7c497
sanespinoz <san.espinoz@gmail.com>
2025-07-12 21:27:18 -0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Cambiado a raíz para pruebas locales
+  base: '/react-quiz/', // Cambiado a raíz para pruebas locales
   plugins: [react()],
 });
