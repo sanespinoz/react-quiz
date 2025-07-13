69b158357ea3f8760ad891306832bdf8303902db
sanespinoz <san.espinoz@gmail.com>
2025-07-12 22:33:38 -0300

    Fix vite base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-quiz/', // Cambiado a raíz para pruebas locales
+  //base: '/', // Cambiado a raíz para pruebas locales
+  //base: '/react-quiz/', // Cambiado a raíz para Github Pages
+  base: './', // Cambiado a raíz para Vercel
   plugins: [react()],
 });
